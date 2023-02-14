60cc38aef67868968cb9b4c1ec8d370e69cf3a4d
calbon2702 <callum.boniface@mirrorweb.com>
2023-02-14 18:08:45 +0000

    feature: url generation

--- data.py
+++ data.py
@@ -1,12 +1,27 @@
 import requests
 
+# https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
+
 class DataCollection:
-    def __init__(self, q_num, q_diff):
-        self.q_num = q_num
-        self.q_diff = q_diff
+    def __init__(self, q_diff=""):
+        self.q_num = "10"
+        self.q_diff = ""
         self.q_category_json = {}
         self.q_category_name = ""
         self.q_category_id = ""
+        self.q_url = ""
+
+    def set_num(self):
+        num = input("How many questions would you like? (Max 50)\n")
+
+        if num == "":
+            self.q_num = "10"
+
+        else:
+            self.q_num = num
+    
+    def set_diff(self):
+        self.q_diff = input("Select difficulty of questions (easy/medium/hard or skip for any):\n")
 
     def get_full_category_json(self):
         category_json = requests.get("https://opentdb.com/api_category.php").json()
@@ -20,7 +35,8 @@ class DataCollection:
         for cat in category_json_list:
             print(f"{cat['id']}: {cat['name']}")
 
-    def set_category_json(self, cat_id):
+    def set_category_json(self):
+        cat_id = int(input(f"{self.get_category_list()}\n"))
         category_json = self.get_full_category_json()['trivia_categories']
 
         for cat in category_json:
@@ -36,3 +52,14 @@ class DataCollection:
 
     def set_category_id(self, id):
         self.q_category_id = id
+
+    def set_url(self):
+        url = "https://opentdb.com/api.php?"
+        url += f"amount={self.q_num}"
+        print(f"SELFNUM: {self.q_num}")
+
+        if self.q_diff:
+            url += f"&difficulty={self.q_diff}"
+
+        if self.q_category_id:
+            url += f"&category={self.q_category_id}"
--- main.py
+++ main.py
@@ -7,11 +7,11 @@ start_url = "https://opentdb.com/api.php?"
 
 while True:
     print("Start of Quiz\n")
-    num_questions = input("How many questions would you like? (Max 50)\n")
-    difficulty = input("Select difficulty of questions (easy/medium/hard or skip for any):\n")
-
-    data = DataCollection(num_questions, difficulty)
-    data.set_category_json(int(input(f"{data.get_category_list()}\n")))
+    data = DataCollection()
+    data.set_num()
+    data.set_diff()
+    data.set_category_json()
+    data.set_url()
 
     # quiz = QuizBrain(question_bank)
 
