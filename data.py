a3b569fe68e5b56817099f24b2963665feac103f
calbon2702 <callum.boniface@mirrorweb.com>
2023-02-14 14:48:46 +0000

    initial commit

---
+++ data.py
@@ -0,0 +1,116 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "correct_answer": "False",
+        "difficulty": "medium",
+        "incorrect_answers": [
+            "True"
+        ],
+        "question": "The US emergency hotline is 911 because of the September 11th terrorist attacks.",
+        "type": "boolean"
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "correct_answer": "8 hours",
+        "difficulty": "hard",
+        "incorrect_answers": [
+            "4 hours",
+            "12 hours",
+            "5 hours"
+        ],
+        "question": "How long are all the cutscenes from Metal Gear Solid 4 (PS3, 2008) combined?",
+        "type": "multiple"
+    },
+    {
+        "category": "General Knowledge",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers": [
+            "False"
+        ],
+        "question": "Cucumbers are usually more than 90% water.",
+        "type": "boolean"
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "correct_answer": "Mario Tennis",
+        "difficulty": "easy",
+        "incorrect_answers": [
+            "Mario Party 3",
+            "Mario Kart: Double Dash!!",
+            "Mario Golf: Toadstool Tour"
+        ],
+        "question": "Which Mario spin-off game did Waluigi make his debut?",
+        "type": "multiple"
+    },
+    {
+        "category": "Entertainment: Film",
+        "correct_answer": "Brighton",
+        "difficulty": "medium",
+        "incorrect_answers": [
+            "Eastbourne",
+            "Mousehole",
+            "Bridlington"
+        ],
+        "question": "In the 1979 British film &quot;Quadrophenia&quot; what is the name of the seaside city the mods are visiting?",
+        "type": "multiple"
+    },
+    {
+        "category": "Entertainment: Television",
+        "correct_answer": "2013",
+        "difficulty": "easy",
+        "incorrect_answers": [
+            "2014",
+            "2016",
+            "2015"
+        ],
+        "question": "When did the TV show Rick and Morty first air on Adult Swim?",
+        "type": "multiple"
+    },
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "correct_answer": "Dispell any esper or magical powers",
+        "difficulty": "medium",
+        "incorrect_answers": [
+            "Teleport",
+            "Make telepathic communications",
+            "Create electricity from his own body"
+        ],
+        "question": "In the &quot;Toaru Majutsu no Index&quot; anime, Touma Kamijou is a level 0 esper that has the ability to do what?",
+        "type": "multiple"
+    },
+    {
+        "category": "Science & Nature",
+        "correct_answer": "Light amplification by stimulated emission of radiation",
+        "difficulty": "easy",
+        "incorrect_answers": [
+            "Lite analysing by stereo ecorazer",
+            "Light amplifier by standby energy of radio",
+            "Life antimatter by standing entry of range"
+        ],
+        "question": "What does LASER stand for?",
+        "type": "multiple"
+    },
+    {
+        "category": "Science: Mathematics",
+        "correct_answer": "7",
+        "difficulty": "easy",
+        "incorrect_answers": [
+            "8",
+            "6",
+            "5"
+        ],
+        "question": "How many sides does a heptagon have?",
+        "type": "multiple"
+    },
+    {
+        "category": "General Knowledge",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers": [
+            "False"
+        ],
+        "question": "Francis Bacon died from a fatal case of pneumonia while he was attempting to preserve meat by stuffing a chicken with snow.",
+        "type": "boolean"
+    }
+]
---
+++ quiz_brain.py
@@ -0,0 +1,24 @@
+class QuizBrain:
+    def __init__(self, lst):
+        self.question_num = 0
+        self.question_lst = lst
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_num < len(self.question_lst)
+
+    def next_question(self):
+        current_question = self.question_lst[self.question_num]
+        self.question_num += 1
+        choice = input(f"Q.{self.question_num}: {current_question.text}\n")
+        self.check_answer(choice, current_question.answer)
+
+    def check_answer(self, choice, answer):
+        if choice.lower() == answer.lower():
+            self.score += 1
+            print("Correct answer!")
+        else:
+            print("Incorrect answer!")
+        
+        print(f"The correct answer is {answer}")
+        print(f"Score: {self.score}/{self.question_num}")
