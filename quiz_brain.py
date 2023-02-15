aa59151bf61ed91c1951c9da408e4888de8e9b4c
calbon2702 <callum.boniface@mirrorweb.com>
2023-02-15 13:24:03 +0000

    feature: multiple choice functionality

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,5 @@
+import random
+
 class QuizBrain:
     def __init__(self, lst):
         self.question_num = 0
@@ -10,7 +12,12 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_lst[self.question_num]
         self.question_num += 1
-        choice = input(f"Q.{self.question_num}: {current_question.text}\n")
+        if current_question.type == "boolean":
+            choice = input(f"Q.{self.question_num}: {current_question.text} (true/false)\n")
+
+        if current_question.type == "multiple":
+            choice = self.multiple_choice(current_question)
+
         self.check_answer(choice, current_question.correct_answer)
 
     def check_answer(self, choice, answer):
@@ -22,3 +29,21 @@ class QuizBrain:
         
         print(f"The correct answer is {answer}")
         print(f"Score: {self.score}/{self.question_num}\n")
+
+    def multiple_choice(self, question):
+        print(f"Q.{self.question_num}: {question.text}\n")
+        answer_lst = question.incorrect_answers + [question.correct_answer]
+        pick_dict = {}
+
+        for i in range(len(answer_lst)):
+            pick = random.choice(answer_lst)
+            print(f"{i + 1}. {pick}")
+            pick_dict[str(i + 1)] = pick
+            answer_lst.remove(pick)
+
+        choice = input()
+
+        if choice in pick_dict.keys():
+            choice = pick_dict[choice]
+
+        return choice
